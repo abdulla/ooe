7f6ba9448a610748935569a449fb044055e46cd5
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-05-15 18:24:56 +1000

    Added write_buffer code for socket.

---
+++ foundation/ipc/socket/write_buffer.hpp
@@ -0,0 +1,45 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#ifndef OOE_FOUNDATION_IPC_SOCKET_WRITE_BUFFER_HPP
+#define OOE_FOUNDATION_IPC_SOCKET_WRITE_BUFFER_HPP
+
+#include "foundation/utility/tuple.hpp"
+
+namespace ooe
+{
+	namespace ipc
+	{
+		namespace socket
+		{
+			typedef tuple< u8*, up_t > buffer_tuple;
+			class write_buffer;
+		}
+	}
+
+//--- ipc::socket::write_buffer ------------------------------------------------
+	class ipc::write_buffer
+	{
+	public:
+		write_buffer( buffer_tuple tuple, up_t size )
+			: internal( tuple._1 >= size ), pointer( internal ? tuple._0 : new u8[ size ] )
+		{
+		}
+
+		~write_buffer( void )
+		{
+			if ( !internal )
+				delete[] pointer;
+		}
+
+		u8* get( void ) const
+		{
+			return pointer;
+		}
+
+	private:
+		bool internal;
+		u8* pointer;
+	};
+}
+
+#endif	// OOE_FOUNDATION_IPC_SOCKET_WRITE_BUFFER_HPP
