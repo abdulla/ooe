e8daeb37800d0190d1c21d41ce0cedb4f1d5a0a0
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-08-02 16:00:30 +1000

    Added fork_io class to aid with forking.
    Fork_io creates forks a process and creates a bi-directional pipe
    between them. It also provides a set of convenience functions for
    cnotrolling communication with the parent or child.

---
+++ foundation/executable/fork_io.cpp
@@ -0,0 +1,142 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#include <cerrno>
+#include <csignal>
+
+#include "foundation/executable/fork_io.hpp"
+#include "foundation/utility/error.hpp"
+#include "foundation/utility/guard.hpp"
+
+namespace ooe
+{
+//--- executable::fork_id ------------------------------------------------------
+	struct executable::fork_id
+	{
+		s32 read;
+		s32 write;
+		pid_t pid;
+
+		fork_id( void );
+		~fork_id( void );
+	};
+
+	executable::fork_id::fork_id( void )
+		: read(), write(), pid()
+	{
+		s32 pipe_1[ 2 ];
+
+		if ( pipe( pipe_1 ) )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to create 1st pipe: " << error::number( errno );
+
+		guard< int ( int ) > guard_1a( close, pipe_1[ 0 ] );
+		guard< int ( int ) > guard_1b( close, pipe_1[ 1 ] );
+		s32 pipe_2[ 2 ];
+
+		if ( pipe( pipe_2 ) )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to create 2nd pipe: " << error::number( errno );
+
+		guard< int ( int ) > guard_2a( close, pipe_2[ 0 ] );
+		guard< int ( int ) > guard_2b( close, pipe_2[ 1 ] );
+		pid = fork();
+
+		if ( pid == -1 )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to fork process: " << error::number( errno );
+		else if ( pid )
+		{
+			guard_1a.clear();
+			guard_2b.clear();
+			read = pipe_1[ 0 ];
+			write = pipe_2[ 1 ];
+		}
+		else
+		{
+			guard_1b.clear();
+			guard_2a.clear();
+			read = pipe_2[ 0 ];
+			write = pipe_1[ 1 ];
+		}
+	}
+
+	executable::fork_id::~fork_id( void )
+	{
+		close( write );
+		close( read );
+	}
+
+//--- executable::fork_io ------------------------------------------------------
+	executable::fork_io::fork_io( void )
+		: id( new fork_id )
+	{
+	}
+
+	executable::fork_io::~fork_io( void )
+	{
+	}
+
+	executable::fork_io::fork_io( const fork_io& copy )
+		: id( copy.id )
+	{
+	}
+
+	executable::fork_io& executable::fork_io::operator =( const fork_io& copy )
+	{
+		id = copy.id;
+		return *this;
+	}
+
+	up_t executable::fork_io::read( void* buffer, up_t bytes )
+	{
+		sp_t read_ = ::read( id->read, buffer, bytes );
+
+		if ( read_ )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to read from process " << id->pid << ": " << error::number( errno );
+
+		return read_;
+	}
+
+	up_t executable::fork_io::write( const void* buffer, up_t bytes )
+	{
+		sp_t wrote = ::write( id->write, buffer, bytes );
+
+		if ( wrote == -1 )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to write to process " << id->pid << ": " << error::number( errno );
+
+		return wrote;
+	}
+
+	executable::fork_io::wait_type executable::fork_io::wait( bool block ) const
+	{
+		s32 status;
+		s32 pid = waitpid( id->pid, &status, block ? 0 : WNOHANG );
+
+		if ( pid == id->pid )
+			return WEXITSTATUS( status ) ? failure : success;
+		else if ( !pid )
+			return waiting;
+		else
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to wait for process " << id->pid << ": " << error::number( errno );
+	}
+
+	void executable::fork_io::signal( s32 number ) const
+	{
+		if ( kill( id->pid, number ) == -1 )
+			throw error::runtime( "executable::fork_io: " ) <<
+				"Unable to signal process " << id->pid << ": " << error::number( errno );
+	}
+
+	bool executable::fork_io::is_child( void ) const
+	{
+		return !id->pid;
+	}
+
+	void executable::fork_io::exit( bool success )
+	{
+		_exit( success ? EXIT_SUCCESS : EXIT_FAILURE );
+	}
+}
---
+++ foundation/executable/fork_io.hpp
@@ -0,0 +1,46 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#ifndef OOE_FOUNDATION_EXECUTABLE_FORK_IO_HPP
+#define OOE_FOUNDATION_EXECUTABLE_FORK_IO_HPP
+
+#include "foundation/utility/fundamental.hpp"
+#include "foundation/utility/pointer.hpp"
+
+namespace ooe
+{
+	namespace executable
+	{
+		struct fork_id;
+		class fork_io;
+	}
+
+	class executable::fork_io
+	{
+	public:
+		enum wait_type
+		{
+			failure,
+			success,
+			waiting
+		};
+
+		fork_io( void );
+		~fork_io( void );
+
+		fork_io( const fork_io& );
+		fork_io& operator =( const fork_io& );
+
+		up_t read( void*, up_t );
+		up_t write( const void*, up_t );
+		wait_type wait( bool = true ) const;
+		void signal( s32 ) const;
+		bool is_child( void ) const;
+
+		static void exit( bool );
+
+	private:
+		shared_ptr< const fork_id > id;
+	};
+}
+
+#endif	// OOE_FOUNDATION_EXECUTABLE_FORK_IO_HPP
