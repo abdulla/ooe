c9ca87a468e5861bd3c9fb29948e80b582a9ba16
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-10-21 00:36:45 +1100

    Added new poll class to IO.
    This allows multiple descriptors to be polled, and each has a callback
    associated with it so an action can be performed on a successful poll.

---
+++ foundation/io/poll.cpp
@@ -0,0 +1,39 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#include <cerrno>
+
+#include "foundation/io/error.hpp"
+#include "foundation/io/poll.hpp"
+
+namespace ooe
+{
+	void poll::insert( const descriptor& desc, const function_type& function )
+	{
+		data.push_back( datum_tuple( &desc, function ) );
+		pollfd event = { desc.illegal_access< true >(), POLLIN, 0 };
+		fds.push_back( event );
+	}
+
+	void poll::wait( void ) const
+	{
+		if ( ::poll( &fds[ 0 ], fds.size(), -1 ) == -1 )
+			throw error::io( "poll: " ) << "Unable to poll descriptors: " << error::number( errno );
+
+		for ( up_t i = 0, end = fds.size(); i != end; ++i )
+		{
+			u16 returned = fds[ i ].revents;
+			const datum_tuple& datum = data[ i ];
+
+			if ( !returned )
+				continue;
+			else if ( returned & POLLERR )
+				datum._1( *datum._0, error );
+			else if ( returned & POLLHUP )
+				datum._1( *datum._0, disconnect );
+			else if ( returned & POLLIN )
+				datum._1( *datum._0, input );
+			else
+				throw error::io( "poll: " ) << "Unknown poll event on descriptor " << fds[ i ].fd;
+		}
+	}
+}
---
+++ foundation/io/poll.hpp
@@ -0,0 +1,41 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#ifndef OOE_FOUNDATION_IO_POLL_HPP
+#define OOE_FOUNDATION_IO_POLL_HPP
+
+#include <vector>
+
+#include <poll.h>
+
+#include "foundation/io/descriptor.hpp"
+#include "foundation/utility/function.hpp"
+
+namespace ooe
+{
+//--- poll ---------------------------------------------------------------------
+	class OOE_VISIBLE poll
+	{
+	public:
+		enum type
+		{
+			error,
+			disconnect,
+			input
+		};
+
+		typedef function< void ( const descriptor&, type ) > function_type;
+
+		void insert( const descriptor&, const function_type& = function_type() );
+		void wait( void ) const;
+
+	private:
+		typedef tuple< const descriptor*, function_type > datum_tuple;
+		typedef std::vector< datum_tuple > datum_vector;
+		typedef std::vector< pollfd > fd_vector;
+
+		datum_vector data;
+		mutable fd_vector fds;
+	};
+}
+
+#endif	// OOE_FOUNDATION_IO_POLL_HPP
