34f62d8c0742cdf8767bc82165a7a5fc7fadbb22
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-10-23 17:57:37 +1100

    Modified semaphore to duplicate descriptor.
    This should hopefully fix the Mac OS X bug, but needs to be tested.
    Currently unable to test, due to being on a Linux machine.

--- platform/darwin/foundation/ipc/semaphore.cpp
+++ platform/darwin/foundation/ipc/semaphore.cpp
@@ -3,16 +3,33 @@
 #include "foundation/io/descriptor.hpp"
 #include "foundation/ipc/semaphore.hpp"
 
+namespace
+{
+	sem_t* create_semaphore( const descriptor& desc )
+	{
+		s32 fd = dup( desc.get() );
+
+		if ( fd == -1 )
+			throw error::runtime( "semaphore: " ) <<
+				"Unable to duplicate descriptor: " << error::number( errno );
+		else if ( fcntl( outfd, F_SETFD, FD_CLOEXEC ) )
+			throw error::runtime( "semaphore: " ) <<
+				"Unable to set close-on-exec: " << error::number( errno );
+
+		return reinterpret_cast< sem_t* >( fd );
+	}
+}
+
 namespace ooe
 {
 //--- ipc::semaphore -----------------------------------------------------------
 	ipc::semaphore::semaphore( const descriptor& desc )
-		: id( 0 ), sem( reinterpret_cast< sem_t* >( desc.get() ) )
+		: id( 0 ), sem( create_semaphore( desc ) )
 	{
 	}
 
 	ipc::semaphore::operator descriptor( void ) const
 	{
-		return reinterpret_cast< up_t >( sem );
+		return dup( reinterpret_cast< up_t >( sem ) );
 	}
 }
