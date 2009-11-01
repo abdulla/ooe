86a0778c1ddc5cf20b1c4b5b82ac86114f2d9622
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-11-01 20:15:15 +1100

    Oops, forgot to add the platform-specific code.

---
+++ platform/darwin/foundation/executable/timer.cpp
@@ -0,0 +1,44 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#include <cerrno>
+
+#include <mach/mach_time.h>
+
+#include "foundation/executable/timer.hpp"
+#include "foundation/utility/error.hpp"
+
+namespace
+{
+	using namespace ooe;
+	mach_timebase_info_data_t timebase = { 0, 0 };
+
+	void initialise_timebase( void )
+	{
+		if ( !timebase.denom && mach_timebase_info( &timebase ) )
+			throw error::runtime( "timer: " ) <<
+			"Unable to get time base: " << error::number( errno );
+	}
+}
+
+namespace ooe
+{
+//--- timer --------------------------------------------------------------------
+	epoch_t timer::epoch( void )
+	{
+		initialise_timebase();
+		u64 time = mach_absolute_time() * timebase.numer / timebase.denom;
+		return epoch_t( time / 1000000000, time % 1000000000 );
+	}
+
+	epoch_t timer::sleep( const epoch_t& value )
+	{
+		initialise_timebase();
+		u64 absolute = mach_absolute_time() +
+			( value._0 * 1000000000 + value._1 ) * timebase.numer / timebase.denom;
+
+		if ( mach_wait_until( absolute ) )
+			throw error::runtime( "timer: " ) << "Unable to sleep: " << error::number( errno );
+
+		return epoch_t( 0, 0 );
+	}
+}
---
+++ platform/posix/foundation/executable/timer.cpp
@@ -0,0 +1,39 @@
+/* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
+
+#include <cerrno>
+#include <ctime>
+
+#include "foundation/executable/timer.hpp"
+#include "foundation/utility/error.hpp"
+
+namespace ooe
+{
+//--- timer --------------------------------------------------------------------
+	epoch_t timer::epoch( void )
+	{
+		timespec value;
+
+		if ( clock_gettime( CLOCK_MONOTONIC_RAW, &value ) )
+			throw error::runtime( "timer: " ) << "Unable to get time: " << error::number( errno );
+
+		return epoch_t( value.tv_sec, value.tv_nsec );
+	}
+
+	epoch_t timer::sleep( const epoch_t& value )
+	{
+		timespec request = { value._0, value._1 };
+
+		if ( request.tv_nsec >= 1000000000 )
+		{
+			request.tv_sec += request.tv_nsec / 1000000000;
+			request.tv_nsec %= 1000000000;
+		}
+
+		timespec remain;
+
+		if ( clock_nanosleep( CLOCK_MONOTONIC, 0, &request, &remain ) )
+			throw error::runtime( "timer: " ) << "Unable to sleep: " << error::number( errno );
+
+		return epoch_t( remain.tv_sec, remain.tv_nsec );
+	}
+}
