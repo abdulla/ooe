848e23d0349c98d9e899910a8c97f5f7014e435e
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-11-01 20:13:32 +1100

    Added platform-specific timer code.
    Timer now uses monotonic clocks on each platform, which should prevent
    odd anomalies with time changes.

--- foundation/executable/timer.cpp
+++ foundation/executable/timer.cpp
@@ -1,11 +1,6 @@
 /* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
 
-#include <cerrno>
-
-#include <sys/time.h>
-
 #include "foundation/executable/timer.hpp"
-#include "foundation/utility/error.hpp"
 
 namespace
 {
@@ -42,33 +37,5 @@ namespace ooe
 	epoch_t timer::get( void ) const
 	{
 		return time;
-	}
-
-	epoch_t timer::epoch( void )
-	{
-		timeval value;
-
-		if ( gettimeofday( &value, 0 ) )
-			throw error::runtime( "timer: " ) << "Unable to get UTC: " << error::number( errno );
-
-		return epoch_t( value.tv_sec, value.tv_usec * 1000 );
-	}
-
-	epoch_t timer::sleep( const epoch_t& time )
-	{
-		epoch_t period( time );
-
-		if ( period._1 >= 1000000000 )
-		{
-			period._0 += period._1 / 1000000000;
-			period._1 %= 1000000000;
-		}
-
-		timespec* spec = reinterpret_cast< timespec* >( &period );
-
-		if ( nanosleep( spec, spec ) )
-			throw error::runtime( "timer: " ) << "Unable to sleep: " << error::number( errno );
-
-		return period;
 	}
 }
