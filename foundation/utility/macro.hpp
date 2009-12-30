7a31ce6991745f8239e6f8e2f640aef78b32a52c
Abdulla Kamar <abdulla.kamar@gmail.com>
2009-12-31 01:10:08 +1100

    Added OOE_LIKELY, OOE_UNLIKELY and OOE_PREFETCH.
    These macros are meant to help with branch prediction and prefetching of
    data.

--- foundation/utility/macro.hpp
+++ foundation/utility/macro.hpp
@@ -4,18 +4,21 @@
 #define OOE_FOUNDATION_UTILITY_MACRO_HPP
 
 #ifdef __GNUC__
-	#define OOE_INLINE			__attribute__( ( __always_inline__ ) )
-	#define OOE_NOINLINE		__attribute__( ( __noinline__ ) )
-	#define OOE_NORETURN		__attribute__( ( __noreturn__ ) )
-	#define OOE_NONNULL			__attribute__( ( __nonnull__ ) )
-	#define OOE_SENTINEL		__attribute__( ( __sentinel__ ) )
-	#define OOE_WARNUNUSED		__attribute__( ( __warn_unused_result__ ) )
-	#define OOE_PURE			__attribute__( ( __pure__ ) )
-	#define OOE_CONST			__attribute__( ( __const__ ) )
-	#define OOE_PACKED			__attribute__( ( __packed__ ) )
-	#define OOE_ALIGN( size )	__attribute__( ( __aligned__( size ) ) )
-	#define OOE_VISIBLE			__attribute__( ( __visibility__( "default" ) ) )
-	#define OOE_HIDDEN			__attribute__( ( __visibility__( "hidden" ) ) )
+	#define OOE_INLINE				__attribute__( ( __always_inline__ ) )
+	#define OOE_NOINLINE			__attribute__( ( __noinline__ ) )
+	#define OOE_NORETURN			__attribute__( ( __noreturn__ ) )
+	#define OOE_NONNULL				__attribute__( ( __nonnull__ ) )
+	#define OOE_SENTINEL			__attribute__( ( __sentinel__ ) )
+	#define OOE_WARNUNUSED			__attribute__( ( __warn_unused_result__ ) )
+	#define OOE_PURE				__attribute__( ( __pure__ ) )
+	#define OOE_CONST				__attribute__( ( __const__ ) )
+	#define OOE_PACKED				__attribute__( ( __packed__ ) )
+	#define OOE_ALIGN( size )		__attribute__( ( __aligned__( size ) ) )
+	#define OOE_VISIBLE				__attribute__( ( __visibility__( "default" ) ) )
+	#define OOE_HIDDEN				__attribute__( ( __visibility__( "hidden" ) ) )
+	#define OOE_LIKELY( boolean )	__builtin_expect( ( boolean ), 1 )
+	#define OOE_UNLIKELY( boolean )	__builtin_expect( ( boolean ), 0 )
+	#define OOE_PREFETCH( address ) __builtin_prefetch( ( address ) )
 #else
 	#define OOE_INLINE
 	#define OOE_NOINLINE
@@ -29,6 +32,9 @@
 	#define OOE_ALIGN( size )
 	#define OOE_VISIBLE
 	#define OOE_HIDDEN
+	#define OOE_LIKELY( boolean )
+	#define OOE_UNLIKELY( boolean )
+	#define OOE_PREFETCH( address )
 #endif
 
 #define OOE_FOURCC( a, b, c, d ) ( a + ( b << 8 ) + ( c << 16 ) + ( d << 24 ) )
