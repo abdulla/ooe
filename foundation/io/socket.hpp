cc1eb73e13b4d9e4cb05e645838462725672dacc
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-01-02 17:48:09 +1100

    Modified to use namespace maros.

--- foundation/io/socket.hpp
+++ foundation/io/socket.hpp
@@ -10,121 +10,123 @@
 struct sockaddr;
 struct addrinfo;
 
-namespace ooe
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+struct address;
+
+//--- socket ---------------------------------------------------------------------------------------
+struct OOE_VISIBLE socket
+	: public platform::socket
 {
-	struct address;
-
-//--- socket -------------------------------------------------------------------
-	struct OOE_VISIBLE socket
-		: public platform::socket
+	enum shutdown_type
 	{
-		enum shutdown_type
-		{
-			read,
-			write,
-			read_write
-		};
-
-		socket( const ooe::descriptor& );
-
-		up_t receive( void*, up_t );
-		up_t send( const void*, up_t );
-
-		void shutdown( shutdown_type );
-		void option( u32, u32 );
-
-		ooe::descriptor receive( void );
-		void send( const ooe::descriptor& );
+		read,
+		write,
+		read_write
 	};
 
-	typedef tuple< socket, socket > socket_pair;
-	socket_pair make_pair( void ) OOE_VISIBLE;
+	socket( const ooe::descriptor& );
 
-//--- listen -------------------------------------------------------------------
-	struct OOE_VISIBLE listen
-		: public socket
+	up_t receive( void*, up_t );
+	up_t send( const void*, up_t );
+
+	void shutdown( shutdown_type );
+	void option( u32, u32 );
+
+	ooe::descriptor receive( void );
+	void send( const ooe::descriptor& );
+};
+
+typedef tuple< socket, socket > socket_pair;
+socket_pair make_pair( void ) OOE_VISIBLE;
+
+//--- listen ---------------------------------------------------------------------------------------
+struct OOE_VISIBLE listen
+	: public socket
+{
+	listen( const address& );
+
+	socket accept( void ) const;
+};
+
+//--- connect --------------------------------------------------------------------------------------
+struct OOE_VISIBLE connect
+	: public socket
+{
+	connect( const address& );
+};
+
+//--- address --------------------------------------------------------------------------------------
+class address
+{
+public:
+	const up_t size;
+
+	u16 family( void ) const;
+	operator const sockaddr*( void ) const;
+
+protected:
+	const shared_array< u8 > data;
+
+	address( up_t );
+};
+
+//--- local_address --------------------------------------------------------------------------------
+struct local_address
+	: public address
+{
+	local_address( const std::string& ) OOE_VISIBLE;
+};
+
+//--- internet_address -----------------------------------------------------------------------------
+struct internet_address
+	: public address
+{
+	internet_address( const addrinfo& ) OOE_VISIBLE;
+};
+
+//--- internet_query_iterator ----------------------------------------------------------------------
+class internet_query_iterator
+	: public boost::iterator_facade< internet_query_iterator, internet_address,
+	boost::forward_traversal_tag, internet_address >
+{
+private:
+	const addrinfo* node;
+
+	internet_query_iterator( const addrinfo* );
+
+	void increment( void ) OOE_VISIBLE;
+	bool equal( const internet_query_iterator& ) const OOE_VISIBLE;
+	internet_address dereference( void ) const OOE_VISIBLE;
+
+	friend class internet_query;
+	friend class boost::iterator_core_access;
+};
+
+//--- internet_query -------------------------------------------------------------------------------
+class OOE_VISIBLE internet_query
+	: private noncopyable
+{
+public:
+	enum type
 	{
-		listen( const address& );
-
-		socket accept( void ) const;
+		any,
+		ipv4,
+		ipv6
 	};
 
-//--- connect ------------------------------------------------------------------
-	struct OOE_VISIBLE connect
-		: public socket
-	{
-		connect( const address& );
-	};
+	typedef internet_query_iterator iterator;
 
-//--- address ------------------------------------------------------------------
-	class address
-	{
-	public:
-		const up_t size;
+	internet_query( const std::string&, const std::string&, type = any );
+	~internet_query( void );
 
-		u16 family( void ) const;
-		operator const sockaddr*( void ) const;
+	iterator begin( void ) const;
+	iterator end( void ) const;
 
-	protected:
-		const shared_array< u8 > data;
+private:
+	addrinfo* head;
+};
 
-		address( up_t );
-	};
-
-//--- local_address ------------------------------------------------------------
-	struct local_address
-		: public address
-	{
-		local_address( const std::string& ) OOE_VISIBLE;
-	};
-
-//--- internet_address ---------------------------------------------------------
-	struct internet_address
-		: public address
-	{
-		internet_address( const addrinfo& );
-	};
-
-//--- internet_query_iterator --------------------------------------------------
-	class internet_query_iterator
-		: public boost::iterator_facade< internet_query_iterator, internet_address,
-		boost::forward_traversal_tag, internet_address >
-	{
-	private:
-		const addrinfo* node;
-
-		internet_query_iterator( const addrinfo* );
-
-		void increment( void );
-		bool equal( const internet_query_iterator& ) const;
-		internet_address dereference( void ) const;
-
-		friend class internet_query;
-		friend class boost::iterator_core_access;
-	};
-
-//--- internet_query -----------------------------------------------------------
-	class internet_query
-	{
-	public:
-		enum type
-		{
-			any,
-			ipv4,
-			ipv6
-		};
-
-		typedef internet_query_iterator iterator;
-
-		internet_query( const std::string&, const std::string&, type = any );
-		~internet_query( void );
-
-		iterator begin( void ) const;
-		iterator end( void ) const;
-
-	private:
-		addrinfo* head;
-	};
-}
+OOE_NAMESPACE_END( ( ooe ) )
 
 #endif	// OOE_FOUNDATION_IO_SOCKET_HPP
--- test/foundation/io/socket_test.cpp
+++ test/foundation/io/socket_test.cpp
@@ -11,87 +11,85 @@
 #include "test/unit/check.hpp"
 #include "test/unit/group.hpp"
 
-namespace
+OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- setup ----------------------------------------------------------------------------------------
+class setup
 {
-	using namespace ooe;
-
-	class setup
+public:
+	setup( void )
+		: pair( make_pair() ), poll( make_pair() )
 	{
-	public:
-		setup( void )
-			: pair( make_pair() ), poll( make_pair() )
-		{
-			u32 value = 0xdeadbeef;
-			descriptor desc( _PATH_TMP "test-file", descriptor::read_write | descriptor::truncate );
-			file( desc ).write( &value, sizeof( value ) );
-			pair._0.send( desc );
-		}
-
-		descriptor receive( void )
-		{
-			return pair._1.receive();
-		}
-
-		socket get( void ) const
-		{
-			return poll._1;
-		}
-
-	private:
-		socket_pair pair;
-		socket_pair poll;
-	};
-
-	typedef unit::group< setup, empty_t, 3 > group_type;
-	typedef group_type::fixture_type fixture_type;
-	group_type group( "socket" );
-}
-
-namespace ooe
-{
-	namespace unit
-	{
-		template<>
-		template<>
-			void fixture_type::test< 0 >( setup& setup )
-		{
-			std::cerr << "send/receive descriptor\n";
-
-			u32 value;
-			file file( setup.receive() );
-			file.seek( 0, file::begin );
-			file.read( &value, sizeof( value ) );
-
-			check( "value == 0xdeadbeef", value == 0xdeadbeef );
-		}
-
-		template<>
-		template<>
-			void fixture_type::test< 1 >( setup& setup )
-		{
-			std::cerr << "poll on shutdown\n";
-
-			socket poll_socket( setup.get() );
-			unique_task< void ( socket::shutdown_type ) >
-				task( make_function( poll_socket, &socket::shutdown ), socket::read );
-
-			poll poll;
-			poll.insert( poll_socket );
-			poll.wait();
-		}
-
-		template<>
-		template<>
-			void fixture_type::test< 2 >( setup& )
-		{
-			std::cerr << "internet query for localhost\n";
-
-			typedef internet_query::iterator iterator_type;
-			internet_query query( "localhost", "http" );
-			up_t j = 1;
-
-			for ( iterator_type i = query.begin(), end = query.end(); i != end; ++i, ++j )
-				std::cout << j << ": internet family " << i->family() << '\n';
-		}
+		u32 value = 0xdeadbeef;
+		descriptor desc( _PATH_TMP "test-file", descriptor::read_write | descriptor::truncate );
+		file( desc ).write( &value, sizeof( value ) );
+		pair._0.send( desc );
 	}
+
+	descriptor receive( void )
+	{
+		return pair._1.receive();
+	}
+
+	socket get( void ) const
+	{
+		return poll._1;
+	}
+
+private:
+	socket_pair pair;
+	socket_pair poll;
+};
+
+typedef unit::group< setup, empty_t, 3 > group_type;
+typedef group_type::fixture_type fixture_type;
+group_type group( "socket" );
+
+OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
+
+OOE_NAMESPACE_BEGIN( ( ooe )( unit ) )
+
+template<>
+template<>
+	void fixture_type::test< 0 >( setup& setup )
+{
+	std::cerr << "send/receive descriptor\n";
+
+	u32 value;
+	file file( setup.receive() );
+	file.seek( 0, file::begin );
+	file.read( &value, sizeof( value ) );
+
+	check( "value == 0xdeadbeef", value == 0xdeadbeef );
 }
+
+template<>
+template<>
+	void fixture_type::test< 1 >( setup& setup )
+{
+	std::cerr << "poll on shutdown\n";
+
+	socket poll_socket( setup.get() );
+	unique_task< void ( socket::shutdown_type ) >
+		task( make_function( poll_socket, &socket::shutdown ), socket::read );
+
+	poll poll;
+	poll.insert( poll_socket );
+	poll.wait();
+}
+
+template<>
+template<>
+	void fixture_type::test< 2 >( setup& )
+{
+	std::cerr << "internet query for localhost\n";
+
+	typedef internet_query::iterator iterator_type;
+	internet_query query( "localhost", "http" );
+	up_t j = 1;
+
+	for ( iterator_type i = query.begin(), end = query.end(); i != end; ++i, ++j )
+		std::cout << j << ": internet family " << i->family() << '\n';
+}
+
+OOE_NAMESPACE_END( ( ooe )( unit ) )
