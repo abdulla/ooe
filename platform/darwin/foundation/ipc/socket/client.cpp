14dcd28dd2d4a0c2adfead984b64b55fbbbd05f6
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-01-04 14:22:06 +1100

    Modified for upcoming changes to socket IPC.

--- platform/darwin/foundation/ipc/socket/client.cpp
+++ platform/darwin/foundation/ipc/socket/client.cpp
@@ -1,12 +1,17 @@
 /* Copyright (C) 2009 Abdulla Kamar. All rights reserved. */
 
 #include "foundation/ipc/socket/client.hpp"
+#include "foundation/utility/error.hpp"
 
-namespace ooe
+OOE_NAMESPACE_BEGIN( ( ooe )( platform )( ipc )( socket ) )
+
+//--- client ---------------------------------------------------------------------------------------
+void client::splice( ooe::socket& socket, up_t size )
 {
-//--- platform::ipc::socket::client --------------------------------------------
-	void platform::ipc::socket::client::splice( ooe::socket& socket, up_t size )
-	{
-		ooe::ipc::socket::header_read( socket, size );
-	}
+	scoped_array< u8 > data( new u8[ size ] );
+
+	if ( socket.receive( data, size ) != size )
+		throw error::runtime( "ipc::socket::client: " ) << "Unable to splice " << size << " bytes";
 }
+
+OOE_NAMESPACE_END( ( ooe )( platform )( ipc )( socket ) )
--- platform/posix/foundation/ipc/socket/client.cpp
+++ platform/posix/foundation/ipc/socket/client.cpp
@@ -2,17 +2,18 @@
 
 #include "foundation/ipc/socket/client.hpp"
 
-namespace ooe
-{
-//--- platform::ipc::socket::client --------------------------------------------
-	platform::ipc::socket::client::client( void )
-		: null( "/dev/null", ooe::descriptor::write )
-	{
-	}
+OOE_NAMESPACE_BEGIN( ( ooe )( platform )( ipc )( socket ) )
 
-	void platform::ipc::socket::client::splice( ooe::socket& socket, up_t size )
-	{
-		if ( socket.splice( null, size ) != size )
-			throw error::runtime( "ipc::socket::client: " ) << "Unable to splice data";
-	}
+//--- client ---------------------------------------------------------------------------------------
+client::client( void )
+	: null( "/dev/null", ooe::descriptor::write )
+{
 }
+
+void client::splice( ooe::socket& socket, up_t size )
+{
+	if ( socket.splice( null, size ) != size )
+		throw error::runtime( "ipc::socket::client: " ) << "Unable to splice " << size << " bytes";
+}
+
+OOE_NAMESPACE_END( ( ooe )( platform )( ipc )( socket ) )
