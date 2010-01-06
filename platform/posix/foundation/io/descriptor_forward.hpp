45d80f1d1dde13a92daedd757c819d37164bb862
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-01-06 16:48:26 +1100

    Added platform-specific headers.

---
+++ platform/posix/foundation/io/descriptor_forward.hpp
@@ -0,0 +1,14 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#ifndef OOE_FOUNDATION_IO_DESCRIPTOR_FORWARD_HPP
+#define OOE_FOUNDATION_IO_DESCRIPTOR_FORWARD_HPP
+
+#include "foundation/executable/environment.hpp"
+
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+const up_t io_alignment = executable::static_page_size;
+
+OOE_NAMESPACE_END( ( ooe ) )
+
+#endif	// OOE_FOUNDATION_IO_DESCRIPTOR_FORWARD_HPP
