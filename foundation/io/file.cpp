a023cfb7bfa53ff1fb2fda22a9c241459cfcfd42
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-01-12 18:17:14 +1100

    Added make_pipe().
    This allows for the easy creation of pipes.

--- foundation/io/file.cpp
+++ foundation/io/file.cpp
@@ -8,73 +8,86 @@
 #include "foundation/io/error.hpp"
 #include "foundation/io/file.hpp"
 
-namespace
+OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
+
+const descriptor& validate( const descriptor& desc )
 {
-	using namespace ooe;
+	descriptor::node_type type = desc.type();
 
-	const descriptor& validate( const descriptor& desc )
-	{
-		if ( desc.type() != descriptor::file && desc.type() != descriptor::character )
-			throw error::io( "file: " ) << "Descriptor is not a file or character device";
+	if ( type != descriptor::file && type != descriptor::pipe && type != descriptor::character )
+		throw error::io( "file: " ) << "Descriptor is not a file, pipe or character device";
 
-		return desc;
-	}
+	return desc;
 }
 
-namespace ooe
+OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
+
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- file -----------------------------------------------------------------------------------------
+file::file( const descriptor& desc )
+	: descriptor( validate( desc ) )
 {
-//--- file ---------------------------------------------------------------------
-	file::file( const descriptor& desc )
-		: descriptor( validate( desc ) )
-	{
-	}
-
-	void file::sync( void ) const
-	{
-		if ( fsync( get() ) )
-			throw error::io( "file: " ) << "File could not be synced: " << error::number( errno );
-	}
-
-	up_t file::read( void* buffer, up_t bytes )
-	{
-		sp_t read_ = ::read( get(), buffer, bytes );
-
-		if ( read_ == -1 )
-			throw error::io( "file: " ) << "Unable to read: " << error::number( errno );
-
-		return read_;
-	}
-
-	up_t file::write( const void* buffer, up_t bytes )
-	{
-		sp_t wrote = ::write( get(), buffer, bytes );
-
-		if ( wrote == -1 )
-			throw error::io( "file: " ) << "Unable to write: " << error::number( errno );
-
-		return wrote;
-	}
-
-	void file::seek( sp_t offset, seek_type point )
-	{
-		s32 whence = SEEK_CUR;
-
-		if ( point == begin )
-			whence = SEEK_SET;
-		else if ( point == end )
-			whence = SEEK_END;
-
-		if ( lseek( get(), offset, whence ) == -1 )
-			throw error::io( "file: " ) << "Unable to seek: " << error::number( errno );
-	}
-
-	up_t file::tell( void ) const
-	{
-		sp_t told = static_cast< s32 >( lseek( get(), 0, SEEK_CUR ) );
-
-		if ( told == -1 )
-			throw error::io( "file: " ) << "Unable to tell: " << error::number( errno );
-
-		return told;
-	}
 }
+
+void file::sync( void ) const
+{
+	if ( fsync( get() ) )
+		throw error::io( "file: " ) << "File could not be synced: " << error::number( errno );
+}
+
+up_t file::read( void* buffer, up_t bytes )
+{
+	sp_t read_ = ::read( get(), buffer, bytes );
+
+	if ( read_ == -1 )
+		throw error::io( "file: " ) << "Unable to read: " << error::number( errno );
+
+	return read_;
+}
+
+up_t file::write( const void* buffer, up_t bytes )
+{
+	sp_t wrote = ::write( get(), buffer, bytes );
+
+	if ( wrote == -1 )
+		throw error::io( "file: " ) << "Unable to write: " << error::number( errno );
+
+	return wrote;
+}
+
+void file::seek( sp_t offset, seek_type point )
+{
+	s32 whence = SEEK_CUR;
+
+	if ( point == begin )
+		whence = SEEK_SET;
+	else if ( point == end )
+		whence = SEEK_END;
+
+	if ( lseek( get(), offset, whence ) == -1 )
+		throw error::io( "file: " ) << "Unable to seek: " << error::number( errno );
+}
+
+up_t file::tell( void ) const
+{
+	sp_t told = static_cast< s32 >( lseek( get(), 0, SEEK_CUR ) );
+
+	if ( told == -1 )
+		throw error::io( "file: " ) << "Unable to tell: " << error::number( errno );
+
+	return told;
+}
+
+//--- make_pipe ------------------------------------------------------------------------------------
+file_pair make_pipe( void )
+{
+	s32 fd[ 2 ];
+
+	if ( pipe( fd ) )
+		throw error::io( "make_pipe: " ) << "Unable to create pipe: " << error::number( errno );
+
+	return file_pair( descriptor( fd[ 0 ] ), descriptor( fd[ 1 ] ) );
+}
+
+OOE_NAMESPACE_END( ( ooe ) )
--- foundation/io/file.hpp
+++ foundation/io/file.hpp
@@ -5,28 +5,34 @@
 
 #include "foundation/io/descriptor.hpp"
 
-namespace ooe
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- file -----------------------------------------------------------------------------------------
+struct OOE_VISIBLE file
+	: public descriptor
 {
-	struct OOE_VISIBLE file
-		: public descriptor
+	enum seek_type
 	{
-		enum seek_type
-		{
-			current,
-			begin,
-			end
-		};
-
-		file( const descriptor& );
-
-		void sync( void ) const;
-
-		up_t read( void*, up_t );
-		up_t write( const void*, up_t );
-
-		void seek( sp_t, seek_type = current );
-		up_t tell( void ) const;
+		current,
+		begin,
+		end
 	};
-}
+
+	file( const descriptor& );
+
+	void sync( void ) const;
+
+	up_t read( void*, up_t );
+	up_t write( const void*, up_t );
+
+	void seek( sp_t, seek_type = current );
+	up_t tell( void ) const;
+};
+
+//--- make_pipe ------------------------------------------------------------------------------------
+typedef tuple< file, file > file_pair;
+file_pair make_pipe( void ) OOE_VISIBLE;
+
+OOE_NAMESPACE_END( ( ooe ) )
 
 #endif	// OOE_FOUNDATION_IO_FILE_HPP
