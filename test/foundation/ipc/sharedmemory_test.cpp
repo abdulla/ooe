c7b3d4b68c9b129cf6db86066a9527629d330a11
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-03-04 16:49:14 +1100

    Added test for shared memory address mapping.

---
+++ test/foundation/ipc/sharedmemory_test.cpp
@@ -0,0 +1,31 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#include "foundation/executable/environment.hpp"
+#include "foundation/ipc/shared_memory.hpp"
+#include "test/unit/check.hpp"
+#include "test/unit/group.hpp"
+
+OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
+
+typedef unit::group< anonymous_t, anonymous_t, 1 > group_type;
+typedef group_type::fixture_type fixture_type;
+group_type group( "shared memory" );
+
+OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
+
+OOE_NAMESPACE_BEGIN( ( ooe )( unit ) )
+
+template<>
+template<>
+	void fixture_type::test< 0 >( anonymous_t& )
+{
+	std::cerr << "mapping the same shared memory region twice\n";
+
+	const char path[] = "/ooe.test.shared-memory";
+	ipc::shared_memory memory_1( path, ipc::shared_memory::create, executable::static_page_size );
+	ipc::shared_memory memory_2( path );
+
+	check( "memory_1 != memory_2", memory_1.get() != memory_2.get() );
+}
+
+OOE_NAMESPACE_END( ( ooe )( unit ) )
