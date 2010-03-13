c931062b1cbc4e2003e5d456c1c5e787de0c4e0b
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-03-14 02:20:53 +1100

    Added symbols needed for texture handling.

--- foundation/opengl_next/symbol.cpp
+++ foundation/opengl_next/symbol.cpp
@@ -23,6 +23,9 @@ void load_symbols( void )
 
 	GenTextures = find< GenTextures_t >( "glGenTextures" );
 	DeleteTextures = find< DeleteTextures_t >( "glDeleteTextures" );
+	BindTexture = find< BindTexture_t >( "glBindTexture" );
+	TexImage2D = find< TexImage2D_t >( "glTexImage2D" );
+	CompressedTexImage2D = find< CompressedTexImage2D_t >( "glCompressedTexImage2D" );
 
 	/*const c8* extensions = */GetString( EXTENSIONS );
 }
@@ -33,5 +36,8 @@ Clear_t* Clear;
 
 GenTextures_t* GenTextures;
 DeleteTextures_t* DeleteTextures;
+BindTexture_t* BindTexture;
+TexImage2D_t* TexImage2D;
+CompressedTexImage2D_t* CompressedTexImage2D;
 
 OOE_NAMESPACE_END( ( ooe )( opengl ) )
--- foundation/opengl_next/symbol.hpp
+++ foundation/opengl_next/symbol.hpp
@@ -13,11 +13,51 @@ void load_symbols( void );
 //--- constants ------------------------------------------------------------------------------------
 enum
 {
-	EXTENSIONS			= 0x1F03,
+	
+	TEXTURE_2D					= 0x0DE1,
 
-	DEPTH_BUFFER_BIT	= 0x00000100,
-	STENCIL_BUFFER_BIT	= 0x00000400,
-	COLOR_BUFFER_BIT	= 0x00004000
+	UNSIGNED_BYTE				= 0x1401,
+	UNSIGNED_SHORT				= 0x1403,
+	UNSIGNED_INT				= 0x1405,
+	FLOAT						= 0x1406,
+	HALF_FLOAT					= 0x140B,
+
+	ALPHA						= 0x1906,
+	RGB							= 0x1907,
+	RGBA						= 0x1908,
+	LUMINANCE					= 0x1909,
+	LUMINANCE_ALPHA				= 0x190A,
+
+	EXTENSIONS					= 0x1F03,
+
+	UNSIGNED_INT_8_8_8_8		= 0x8035,
+	ALPHA8						= 0x803C,
+	LUMINANCE8					= 0x8040,
+	LUMINANCE8_ALPHA8			= 0x8045,
+	RGB8						= 0x8051,
+	RGBA8						= 0x8058,
+
+	BGR							= 0x80E0,
+	BGRA						= 0x80E1,
+
+	COMPRESSED_RGBA_S3TC_DXT1	= 0x83F1,
+	COMPRESSED_RGBA_S3TC_DXT3	= 0x83F2,
+	COMPRESSED_RGBA_S3TC_DXT5	= 0x83F3,
+
+	RGBA32F						= 0x8814,
+	RGB32F						= 0x8815,
+	ALPHA32F					= 0x8816,
+	LUMINANCE32F				= 0x8818,
+	LUMINANCE_ALPHA32F			= 0x8819,
+	RGBA16F						= 0x881A,
+	RGB16F						= 0x881B,
+	ALPHA16F					= 0x881C,
+	LUMINANCE16F				= 0x881E,
+	LUMINANCE_ALPHA16F			= 0x881F,
+
+	DEPTH_BUFFER_BIT			= 0x00000100,
+	STENCIL_BUFFER_BIT			= 0x00000400,
+	COLOR_BUFFER_BIT			= 0x00004000
 };
 
 //--- typedefs -------------------------------------------------------------------------------------
@@ -26,6 +66,9 @@ typedef void ( Clear_t )( u32 );
 
 typedef void ( GenTextures_t )( s32, u32* );
 typedef void ( DeleteTextures_t )( s32, const u32* );
+typedef void ( BindTexture_t )( u32, u32 );
+typedef void ( TexImage2D_t )( u32, s32, s32, s32, s32, s32, u32, u32, const void* );
+typedef void ( CompressedTexImage2D_t )( u32, s32, u32, s32, s32, s32, s32, const void* );
 
 //--- functions ------------------------------------------------------------------------------------
 extern GetString_t* GetString;
@@ -33,6 +76,9 @@ extern Clear_t* Clear;
 
 extern GenTextures_t* GenTextures;
 extern DeleteTextures_t* DeleteTextures;
+extern BindTexture_t* BindTexture;
+extern TexImage2D_t* TexImage2D;
+extern CompressedTexImage2D_t* CompressedTexImage2D;
 
 OOE_NAMESPACE_END( ( ooe )( opengl ) )
 
