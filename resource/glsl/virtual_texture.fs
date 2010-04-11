43f251a30275bbf6c1c3d5bc3c103d3ff3752de1
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-04-12 00:58:21 +1000

    Minor modification (removed a comment).

--- resource/glsl/virtual_texture.fs
+++ resource/glsl/virtual_texture.fs
@@ -1,6 +1,5 @@
 vec4 virtual_texture( sampler2D page_table, sampler2D page_cache, vec2 virtual_address )
 {
-	// TODO: loop upwards if entry.z = -1
 	// entry = { x: physical-x, y: physical-y, z: 2^mipmap-level }
 	vec4 entry = texture2D( page_table, virtual_address );
 	vec2 physical_address = entry.xy + fract( virtual_address * entry.z );
