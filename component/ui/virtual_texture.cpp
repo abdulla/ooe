9872df922251910099a653c193a6c3d5789bbde0
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-04-16 17:47:54 +1000

    Added support for page locking and unlocking.

--- component/ui/virtual_texture.cpp
+++ component/ui/virtual_texture.cpp
@@ -9,6 +9,7 @@
 
 OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
 
+typedef tuple< u32, u32, u32, u32 > region_type;
 const image::type table_format = image::rgb_f32;
 
 u32 table_width( const device_type& device, const physical_source& source )
@@ -75,6 +76,27 @@ void write_pyramid( image_pyramid& pyramid, const virtual_texture::key_type& key
 	rgb[ 2 ] = pow_level;
 }
 
+region_type get_region( const physical_source& source, u8 level_limit,
+	u32 x, u32 y, u32 width, u32 height, u8 level )
+{
+	u32 size = source.size();
+
+	if ( level > level_limit )
+		throw error::runtime( "virtual_texture: " ) <<
+			"Mipmap level " << level << " > maximum level " << level_limit;
+	else if ( x + width > size || y + height > size )
+		throw error::runtime( "virtual_texture: " ) <<
+			"Load of (" << x << ", " << y << ", " << width << ", " << height <<
+			") is outside of source size " << size << " at level " << level;
+
+	u32 page_size = source.page_size();
+	u32 page_x1 = floor( x >> level, page_size );
+	u32 page_y1 = floor( y >> level, page_size );
+	u32 page_x2 = ceiling( ( x + width ) >> level, page_size );
+	u32 page_y2 = ceiling( ( y + height ) >> level, page_size );
+	return region_type( page_x1, page_x2, page_y1, page_y2 );
+}
+
 OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
 
 OOE_NAMESPACE_BEGIN( ( ooe ) )
@@ -91,55 +113,56 @@ virtual_texture::virtual_texture( const device_type& device, physical_source& so
 	for ( u32 y = 0; y != cache_size; y += page_size )
 	{
 		for ( u32 x = 0; x != cache_size; x += page_size )
-			list.push_back( value_type( x, y, key_type( 0, 0, 255 ) ) );
+			list.push_back( value_type( x, y, key_type( 0, 0, 255 ), false ) );
 	}
+
+	u8 level = pyramid.size() - 1;
+	u32 size = source.size();
+	load( 0, 0, size, size, level, true );
+	write();
 }
 
 void virtual_texture::input( const std::string& name, block_type& block ) const
 {
 	u16 page_size = source.page_size();
+	f32 page_log2 = log2( page_size ) - 1;	// minus one to force more detail
 	block->input( name + ".page_ratio", divide( page_size, cache_size ) );
-	block->input( name + ".page_log2", f32( log2( page_size ) ) );
+	block->input( name + ".page_log2", page_log2 );
 	block->input( name + ".page_cache", cache );
 	block->input( name + ".page_table", table );
 }
 
-void virtual_texture::load( u32 x, u32 y, u32 width, u32 height, u8 level )
+void virtual_texture::load( u32 x, u32 y, u32 width, u32 height, u8 level, bool locked )
 {
 	u8 level_limit = pyramid.size() - 1;
-	u32 size = source.size();
-
-	if ( level > level_limit )
-		throw error::runtime( "virtual_texture: " ) <<
-			"Mipmap level " << level << " > maximum level " << level_limit;
-	else if ( x + width > size || y + height > size )
-		throw error::runtime( "virtual_texture: " ) <<
-			"Load of (" << x << ", " << y << ", " << width << ", " << height <<
-			") is outside of source size " << size << " at level " << level;
-
-	u32 page_size = source.page_size();
-	u32 page_x1 = floor( x >> level, page_size );
-	u32 page_y1 = floor( y >> level, page_size );
-	u32 page_x2 = ceiling( ( x + width ) >> level, page_size );
-	u32 page_y2 = ceiling( ( y + height ) >> level, page_size );
+	region_type region = get_region( source, level_limit, x, y, width, height, level );
 	bool do_write = false;
 
 	// TODO: limit the load by some number of texels
-	for ( u32 j = page_y1; j != page_y2; ++j )
+	for ( u32 j = region._2; j != region._3; ++j )
 	{
-		for ( u32 i = page_x1; i != page_x2; ++i )
+		for ( u32 i = region._0; i != region._1; ++i )
 		{
 			key_type key( i, j, level );
 			cache_map::iterator find = map.find( key );
+			cache_list::iterator end = list.end();
 
+			// check if page has already been loaded
 			if ( find != map.end() )
 			{
-				list.splice( list.end(), list, find->second );
+				find->second->_3 = locked;
+				list.splice( end, list, find->second );
 				continue;
 			}
 
+			// find least-recently-used unlocked page
 			cache_list::iterator page = list.begin();
+			for ( ; page != end && page->_3; ++page ) {}
 
+			if ( page == end )
+				throw error::runtime( "virtual_texture: " ) << "All pages are locked";
+
+			// if page has been previously assigned, evict entry
 			if ( page->_2._2 != 255 )
 			{
 				map.erase( page->_2 );
@@ -148,9 +171,11 @@ void virtual_texture::load( u32 x, u32 y, u32 width, u32 height, u8 level )
 
 			do_write = true;
 			page->_2 = key;
+			page->_3 = locked;
 			map.insert( cache_map::value_type( page->_2, page ) );
-			list.splice( list.end(), list, page );
+			list.splice( end, list, page );
 
+			// TODO: background loading
 			cache->write( source.read( i, j, level ), page->_0, page->_1 );
 
 			f32 table_x = divide( page->_0, cache_size );
@@ -162,6 +187,23 @@ void virtual_texture::load( u32 x, u32 y, u32 width, u32 height, u8 level )
 
 	if ( do_write )
 		bitset.set( level );
+}
+
+void virtual_texture::unlock( u32 x, u32 y, u32 width, u32 height, u8 level )
+{
+	u8 level_limit = pyramid.size() - 1;
+	region_type region = get_region( source, level_limit, x, y, width, height, level );
+
+	for ( u32 j = region._2; j != region._3; ++j )
+	{
+		for ( u32 i = region._0; i != region._1; ++i )
+		{
+			cache_map::iterator find = map.find( key_type( i, j, level ) );
+
+			if ( find != map.end() )
+				find->second->_3 = false;
+		}
+	}
 }
 
 void virtual_texture::write( void )
--- component/ui/virtual_texture.hpp
+++ component/ui/virtual_texture.hpp
@@ -35,11 +35,12 @@ public:
 	virtual_texture( const device_type&, physical_source& );
 
 	void input( const std::string&, block_type& ) const;
-	void load( u32, u32, u32, u32, u8 );
+	void load( u32, u32, u32, u32, u8, bool = false );
+	void unlock( u32, u32, u32, u32, u8 );
 	void write( void );
 
 private:
-	typedef tuple< u32, u32, key_type > value_type;
+	typedef tuple< u32, u32, key_type, bool > value_type;
 	typedef std::list< value_type > cache_list;
 	typedef std::map< key_type, cache_list::iterator > cache_map;
 	typedef std::bitset< sizeof( up_t ) > cache_bitset;
