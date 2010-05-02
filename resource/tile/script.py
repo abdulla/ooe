c8208d3a80de5a4ea7377e4a04c4c595188aa2b5
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-05-02 15:57:53 +1000

    Modified tile script to auto-crop/resize images.

--- resource/tile/script.py
+++ resource/tile/script.py
@@ -16,19 +16,30 @@ if not options.file or not options.directory:
 	parser.error( 'Both directory and file must be specified' )
 
 if __name__ == '__main__':
+	stdout = sys.stdout
+	sys.stdout = sys.stderr
+
 	image = Image.open( options.file )
+	type = image.format.lower()
 	page_size = options.page_size
 	image_size = image.size[ 0 ]
 
 	if image_size != image.size[ 1 ]:
-		sys.exit( 'Image is not square:', image_size )
-	elif image_size % page_size:
-		sys.exit( 'Image size', image_size, 'is not divisiable by page size', page_size )
+		image_size = min( image.size )
+		print 'Cropping image from size', image.size, 'to', ( image_size, image_size ), '...',
+		image = image.crop( ( 0, 0, image_size, image_size ) )
+		print 'done.'
+
+	if image_size % page_size:
+		bit_round_down = 2 ** int( math.floor( math.log( image_size, 2 ) ) )
+		print 'Resizing image from size', image_size, 'to', bit_round_down, '...',
+		image = image.resize( ( bit_round_down, bit_round_down ) )
+		image_size = bit_round_down
+		print 'done.'
 
 	metadata = open( os.path.join( options.directory, 'metadata' ), 'w' )
-	type = image.format.lower()
 	metadata.write( '%s %u %u' % ( type, page_size, image_size ) )
-	level_limit = int( math.log( image_size / page_size, 2 ) )
+	level_limit = int( math.log( image_size / page_size, 2 ) ) + 1
 
 	print 'Processing "%s":' % options.file
 	print '\tImage size:   ', image_size
@@ -37,9 +48,9 @@ if __name__ == '__main__':
 	print '\tMipmap levels:', level_limit
 
 	for level in xrange( level_limit ):
-		level_size = image_size >> level
-		resize = image.resize( ( level_size, level_size ) )
 		sys.stderr.write( '\n\tMipmap level %u: ' % level )
+		level_size = image_size >> level
+		image = image.resize( ( level_size, level_size ) )
 
 		for y in xrange( 0, level_size, page_size ):
 			for x in xrange( 0, level_size, page_size ):
