708819115e7b57e8eb2ed0f307b035c75f23bc4c
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-05-11 22:05:07 +1000

    Added tile_source to load large, tiled image data.

---
+++ component/ui/tile_source.cpp
@@ -0,0 +1,94 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#include <cstdlib>
+
+#include "foundation/io/memory.hpp"
+#include "component/ui/tile_source.hpp"
+
+OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
+
+typedef std::vector< std::string > vector_type;
+typedef std::string::const_iterator iterator_type;
+
+vector_type words( iterator_type i, const iterator_type& end )
+{
+	vector_type vector;
+
+	while ( i != end )
+	{
+		while ( i != end && isspace( *i ) )
+			++i;
+
+		iterator_type begin = i;
+
+		while ( i != end && !isspace( *i ) )
+			++i;
+
+		vector.push_back( std::string( begin, i ) );
+	}
+
+	return vector;
+}
+
+OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
+
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- tile_source ----------------------------------------------------------------------------------
+tile_source::tile_source( const std::string& root_, const decoder_map& map )
+	: root( root_ ), type(), decoder(), size_(), format_(), page_size_(), level_limit()
+{
+	memory memory( root + "/metadata" );
+	std::string metadata( memory.as< c8 >(), memory.size() );
+	vector_type vector = words( metadata.begin(), --metadata.end() );
+
+	if ( vector.size() != 2 )
+		throw error::runtime( "tile_source: " ) << "Invalid metadata";
+
+	decoder_map::const_iterator i = map.find( vector[ 0 ] );
+
+	if ( i == map.end() )
+		throw error::runtime( "tile_source: " ) <<
+			"Unable to find decoder for image type \"" << vector[ 0 ] << "\"";
+
+	type = i->first;
+	decoder = i->second;
+	s32 levels = std::atoi( vector[ 1 ].c_str() );
+
+	if ( !levels )
+		throw error::runtime( "tile_source: " ) << "Invalid number of mipmap levels: " << levels;
+
+	level_limit = levels - 1;
+	image image = decoder( root + "/0_0_0." + i->first );
+	size_ = image.width << level_limit;
+	format_ = image.format;
+	page_size_ = image.width;
+}
+
+tile_source::~tile_source( void )
+{
+}
+
+u32 tile_source::size( void ) const
+{
+	return size_;
+}
+
+image::type tile_source::format( void ) const
+{
+	return format_;
+}
+
+u16 tile_source::page_size( void ) const
+{
+	return page_size_;
+}
+
+image tile_source::read( u32 x, u32 y, u8 level )
+{
+	std::string path( root );
+	path << '/' << x << '_' << y << '_' << level_limit - level << '.' << type;
+	return decoder( path );
+}
+
+OOE_NAMESPACE_END( ( ooe ) )
--- component/ui/tile_source.hpp
+++ component/ui/tile_source.hpp
@@ -1,27 +1,39 @@
 /* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
 
-#ifndef OOE_COMPONENT_UI_FONT_SOURCE_HPP
-#define OOE_COMPONENT_UI_FONT_SOURCE_HPP
+#ifndef OOE_COMPONENT_UI_TILE_SOURCE_HPP
+#define OOE_COMPONENT_UI_TILE_SOURCE_HPP
 
 #include "component/ui/virtual_texture.hpp"
 
 OOE_NAMESPACE_BEGIN( ( ooe ) )
 
-typedef std::map< std::string, image ( * )( const descriptor& ) > decoder_map;
+typedef std::map< std::string, decoder_type > decoder_map;
 
 //--- tile_source ----------------------------------------------------------------------------------
 class tile_source
 	: public physical_source
 {
 public:
-	tile_source( const std::string&, const decode_map& );
+	tile_source( const std::string&, const decoder_map& ) OOE_VISIBLE;
 	virtual ~tile_source( void );
+
+	virtual u32 size( void ) const;
+	virtual image::type format( void ) const;
+	virtual u16 page_size( void ) const;
 
 private:
 	const std::string root;
-	const decode_map& map;
+	std::string type;
+	decoder_type decoder;
+
+	u32 size_;
+	image::type format_;
+	u16 page_size_;
+	u8 level_limit;
+
+	virtual image read( u32, u32, u8 );
 };
 
 OOE_NAMESPACE_END( ( ooe ) )
 
-#endif	// OOE_COMPONENT_UI_FONT_SOURCE_HPP
+#endif	// OOE_COMPONENT_UI_TILE_SOURCE_HPP
