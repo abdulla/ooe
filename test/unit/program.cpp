c41d08eb41d969ea8be2bc2fc8920347a14b94a5
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-09-12 05:31:44 +1000

    Modified unit program to use namespace macros.

--- test/unit/program.cpp
+++ test/unit/program.cpp
@@ -8,76 +8,73 @@
 #include "test/unit/program.hpp"
 #include "test/unit/runner.hpp"
 
-namespace
+OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
+
+bool unit_launch( const std::string&, const std::string& name, s32 argc, c8** argv )
 {
-	using namespace ooe;
+	::signal( SIGCHLD, SIG_DFL );
+	time_t timeout = 60;
+	bool no_stdout = true;
 
-	bool unit_launch( const std::string&, const std::string& name, s32 argc, c8** argv )
+	for ( s32 option; ( option = getopt( argc, argv, "lst:" ) ) != -1; )
 	{
-		::signal( SIGCHLD, SIG_DFL );
-		time_t timeout = 60;
-		bool no_stdout = true;
-
-		for ( s32 option; ( option = getopt( argc, argv, "lst:" ) ) != -1; )
+		switch ( option )
 		{
-			switch ( option )
-			{
-			case 'l':
-				std::cout << "Available groups of tests:\n";
+		case 'l':
+			std::cout << "Available groups of tests:\n";
 
-				for ( unit::runner::iterator_type i = unit::global_runner.begin(),
-					end = unit::global_runner.end(); i != end; ++i )
-					std::cout << "    " << i->first << '\n';
+			for ( unit::runner::iterator_type i = unit::global_runner.begin(),
+				end = unit::global_runner.end(); i != end; ++i )
+				std::cout << "    " << i->first << '\n';
 
-				return true;
+			return true;
 
-			case 's':
-				no_stdout = false;
-				break;
+		case 's':
+			no_stdout = false;
+			break;
 
-			case 't':
-				timeout = std::strtoul( optarg, 0, 10 );
-				break;
+		case 't':
+			timeout = std::strtoul( optarg, 0, 10 );
+			break;
 
-			default:
-				std::cout <<
-					"Usage:\n"
-					"    " << name << " [-t timeout] [test_group]...\n"
-					"    " << name << " -l\n"
-					"\n"
-					"Options:\n"
-					"    -s             Enable stdout in tests\n"
-					"    -t <timeout>  Time out for each test\n"
-					"    -l             List all groups of tests\n";
+		default:
+			std::cout <<
+				"Usage:\n"
+				"    " << name << " [-t timeout] [test_group]...\n"
+				"    " << name << " -l\n"
+				"\n"
+				"Options:\n"
+				"    -s             Enable stdout in tests\n"
+				"    -t <timeout>  Time out for each test\n"
+				"    -l             List all groups of tests\n";
 
-				return false;
-			}
+			return false;
 		}
-
-		if ( optind == argc )
-			return unit::global_runner.run( timeout, no_stdout );
-
-		bool success = true;
-
-		for ( s32 i = optind; i != argc; ++i )
-		{
-			if ( !unit::global_runner.run( argv[ i ], timeout, no_stdout ) )
-				success = false;
-		}
-
-		return success;
 	}
+
+	if ( optind == argc )
+		return unit::global_runner.run( timeout, no_stdout );
+
+	bool success = true;
+
+	for ( s32 i = optind; i != argc; ++i )
+	{
+		if ( !unit::global_runner.run( argv[ i ], timeout, no_stdout ) )
+			success = false;
+	}
+
+	return success;
 }
 
-namespace ooe
-{
-	namespace unit
-	{
-		runner global_runner;
-	}
+OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
 
-	s32 unit::launch( s32 argc, c8** argv )
-	{
-		return executable::launch( unit_launch, argc, argv );
-	}
+OOE_NAMESPACE_BEGIN( ( ooe )( unit ) )
+
+runner global_runner;
+
+s32 launch( s32 argc, c8** argv )
+{
+	return executable::launch( unit_launch, argc, argv );
 }
+
+OOE_NAMESPACE_END( ( ooe )( unit ) )
