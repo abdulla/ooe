756268eda0e76681784ddb9550ea09e6e207c415
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-09-24 09:56:47 +1000

    Modified program to use scoped.

--- platform/darwin/foundation/executable/program.mm
+++ platform/darwin/foundation/executable/program.mm
@@ -1,41 +1,46 @@
 /* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
 
+#include <objc/message.h>
 #include <Foundation/Foundation.h>
 #include <mach-o/dyld.h>
 
 #include "foundation/executable/program.hpp"
 #include "foundation/utility/error.hpp"
+#include "foundation/utility/scoped.hpp"
 
-namespace ooe
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- executable -----------------------------------------------------------------------------------
+bool executable::path( c8* buffer, up_t size )
 {
-//--- executable ---------------------------------------------------------------
-	bool executable::path( c8* buffer, up_t size )
-	{
-		u32 length = size;
-		return !_NSGetExecutablePath( buffer, &length );
-	}
-
-//--- platform -----------------------------------------------------------------
-	bool platform::launch( executable::launch_type launch,
-		const std::string& root, const std::string& name, s32 argc, c8** argv )
-	{
-		bool status = false;
-		NSAutoreleasePool* pool = [ [ NSAutoreleasePool alloc ] init ];
-
-		if ( !pool )
-			throw error::runtime( "executable: " ) << "Unable to initialise auto-release pool";
-
-		@try
-		{
-			status = launch( root, name, argc, argv );
-		}
-		@catch ( NSException* error )
-		{
-			throw error::runtime( error.name.UTF8String ) << ": " << error.reason.UTF8String;
-		}
-
-		[ pool release ];
-		return status;
-	}
+	u32 length = size;
+	return !_NSGetExecutablePath( buffer, &length );
 }
 
+//--- platform -------------------------------------------------------------------------------------
+bool platform::launch( executable::launch_type launch,
+	const std::string& root, const std::string& name, s32 argc, c8** argv )
+{
+	bool status = false;
+	NSAutoreleasePool* pool = [ [ NSAutoreleasePool alloc ] init ];
+
+	if ( !pool )
+		throw error::runtime( "executable: " ) << "Unable to initialise auto-release pool";
+
+	typedef id ( send_type )( id, SEL );
+	scoped< send_type >
+		scoped( reinterpret_cast< send_type* >( objc_msgSend ), pool, @selector( release ) );
+
+	@try
+	{
+		status = launch( root, name, argc, argv );
+	}
+	@catch ( NSException* error )
+	{
+		throw error::runtime( error.name.UTF8String ) << ": " << error.reason.UTF8String;
+	}
+
+	return status;
+}
+
+OOE_NAMESPACE_END( ( ooe ) )
