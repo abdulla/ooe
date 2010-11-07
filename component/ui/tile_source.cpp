f2c0b83c03deb004054c40b8efacd788120597f7
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-11-08 01:11:55 +1100

    Modified tile_source to use JSON for metadata.

--- component/ui/tile_source.cpp
+++ component/ui/tile_source.cpp
@@ -1,37 +1,11 @@
 /* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
 
-#include <cstdlib>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 #include "component/ui/tile_source.hpp"
-#include "foundation/io/memory.hpp"
+#include "foundation/io/directory.hpp"
 #include "foundation/utility/error.hpp"
-
-OOE_ANONYMOUS_NAMESPACE_BEGIN( ( ooe ) )
-
-typedef std::vector< std::string > vector_type;
-typedef std::string::const_iterator iterator_type;
-
-vector_type words( iterator_type i, const iterator_type& end )
-{
-    vector_type vector;
-
-    while ( i != end )
-    {
-        while ( i != end && isspace( *i ) )
-            ++i;
-
-        iterator_type begin = i;
-
-        while ( i != end && !isspace( *i ) )
-            ++i;
-
-        vector.push_back( std::string( begin, i ) );
-    }
-
-    return vector;
-}
-
-OOE_ANONYMOUS_NAMESPACE_END( ( ooe ) )
 
 OOE_NAMESPACE_BEGIN( ( ooe ) )
 
@@ -39,29 +13,19 @@ OOE_NAMESPACE_BEGIN( ( ooe ) )
 tile_source::tile_source( const std::string& root_, const decoder_map& map )
     : root( root_ ), type(), decoder(), size_(), format_(), page_size_(), level_limit()
 {
-    // TODO: use boost::ptree for configuration
-    memory memory( root + "/metadata" );
-    std::string metadata( memory.as< c8 >(), memory.size() );
-    vector_type vector = words( metadata.begin(), --metadata.end() );
+    boost::property_tree::ptree pt;
+    read_json( canonical_path( root + "/metadata" ), pt );
 
-    if ( vector.size() != 2 )
-        throw error::runtime( "tile_source: " ) << "Invalid metadata";
-
-    decoder_map::const_iterator i = map.find( vector[ 0 ] );
+    type = pt.get< std::string >( "type" );
+    level_limit = pt.get< u8 >( "level_limit" );
+    decoder_map::const_iterator i = map.find( type );
 
     if ( i == map.end() )
         throw error::runtime( "tile_source: " ) <<
-            "Unable to find decoder for image type \"" << vector[ 0 ] << "\"";
+            "Unable to find decoder for image type \"" << type << "\"";
 
-    type = i->first;
     decoder = i->second;
-    s32 levels = std::atoi( vector[ 1 ].c_str() );
-
-    if ( !levels )
-        throw error::runtime( "tile_source: " ) << "Invalid number of mipmap levels: " << levels;
-
-    level_limit = levels - 1;
-    image image = decoder( root + "/0_0_0." + i->first );
+    image image = decoder( root + "/0_0_0." + type );
     size_ = image.width << level_limit;
     format_ = image.format;
     page_size_ = image.width;
