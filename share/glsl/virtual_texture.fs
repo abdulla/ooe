39d7a37d2ee488863c69af4e1639d250513a2ee1
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-11-08 12:42:10 +1100

    Modified virtual_texture shader to remove warnings.

--- share/glsl/virtual_texture.fs
+++ share/glsl/virtual_texture.fs
@@ -1,16 +1,17 @@
 vec4 vtexture2D( vsampler2D sampler, vec2 virtual_address )
 {
-    vec4 entry;
+    // will default to loading first page if lookup fails to find valid data
+    vec2 entry = vec2( 0, 1 );
 
     // handle page fault by ascending mipmap levels to find valid data
     for ( float bias = sampler.bias_range.x; bias != sampler.bias_range.y; ++bias )
     {
-        entry = texture2D( sampler.page_table, virtual_address, bias );
+        entry = texture2D( sampler.page_table, virtual_address, bias ).xy;
 
-        if ( entry.r > -1. )
+        if ( entry.x > -1. )
             break;
     }
 
-    vec2 physical_address = fract( virtual_address * entry.g );
-    return texture2DArray( sampler.page_cache, vec3( physical_address, entry.r ) );
+    vec2 physical_address = fract( virtual_address * entry.y );
+    return texture2DArray( sampler.page_cache, vec3( physical_address, entry.x ) );
 }
