5b7cfd3b04f400ad071aa773072f50e764ddac7e
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-11-13 15:43:01 +1100

    Fixed bug when using signed 16-bit textures.
    The textures were being uploaded as unsigned, when they should have been
    signed.

--- foundation/opengl/symbol.hpp
+++ foundation/opengl/symbol.hpp
@@ -32,6 +32,7 @@ enum
     TEXTURE_2D                  = 0x0DE1,
 
     UNSIGNED_BYTE               = 0x1401,
+    SHORT                       = 0x1402,
     UNSIGNED_SHORT              = 0x1403,
     UNSIGNED_INT                = 0x1405,
     FLOAT                       = 0x1406,
