fcf2e4dad6b9c47e1700b2c1a4fec9449830eeaa
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-11-21 09:24:58 +1100

    Modified to use new image format functions.

--- component/ui/virtual_texture.cpp
+++ component/ui/virtual_texture.cpp
@@ -13,7 +13,7 @@ OOE_ANONYMOUS_BEGIN( ( ooe ) )
 typedef tuple< u32, u32, u32, u32 > region_type;
 typedef std::pair< page_cache::page_map::iterator, page_cache::page_map::iterator > pair_type;
 const image_format::type table_format = image_format::rg_s16;
-const u8 table_channels = 2;
+const u8 table_subpixels = subpixels( table_format );
 
 u32 check_page_size( u16 page_size )
 {
@@ -45,7 +45,7 @@ image make_image( u32 size )
 {
     image image( size, size, table_format );
 
-    for ( s16* i = image.as< s16 >(), * end = i + pixels( image ) * table_channels; i != end; ++i )
+    for ( s16* i = image.as< s16 >(), * end = i + pixels( image ) * table_subpixels; i != end; ++i )
         *i = -1;
 
     return image;
@@ -76,7 +76,7 @@ texture_array_type make_array( const device_type& device, image_format::type for
 void write_pyramid( image_pyramid& pyramid, const pyramid_index& index, s16 i, s16 exponent )
 {
     u32 width = pyramid.width >> index.level;
-    s16* rg = pyramid[ index.level ].as< s16 >() + ( index.x + index.y * width ) * table_channels;
+    s16* rg = pyramid[ index.level ].as< s16 >() + ( index.x + index.y * width ) * table_subpixels;
     rg[ 0 ] = i;
     rg[ 1 ] = exponent;
 }
