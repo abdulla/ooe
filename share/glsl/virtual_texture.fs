99dc630ed347bf4b74828c27e285e14dc2a1afc4
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-11-26 00:07:06 +1100

    Fixed graphical glitch in virtual_texture.
    It turns out that the implicit derivative calculated in a texture2D() is
    undefined in a branch. This would cause white lines to appear on the
    border of textures when a fault caused vaddress() to lookup a higher
    mipmap level. To fix this, derivatives should be precalculated
    explicitly and use texture2DGrad().

--- share/glsl/virtual_texture.fs
+++ share/glsl/virtual_texture.fs
@@ -1,10 +1,15 @@
 vec3 vaddress( vsampler2D sampler, vec2 virtual_address )
 {
+    vec2 dx = dFdx( virtual_address );
+    vec2 dy = dFdy( virtual_address );
     ivec2 entry = ivec2( -1, -1 );
 
     // handle page fault by ascending mipmap levels to find valid data
     for ( int bias = sampler.bias_range.x; entry.x == -1 && bias != sampler.bias_range.y; ++bias )
-        entry = texture2D( sampler.page_table, virtual_address, float( bias ) ).xy;
+    {
+        float d = exp2( float( -bias ) );
+        entry = texture2DGrad( sampler.page_table, virtual_address, dx / d, dy / d ).xy;
+    }
 
     // default to loading first page if lookup fails to find valid data
     if ( entry.x == -1  )
@@ -23,7 +28,7 @@ vec4 vtexture2D( vsampler2D sampler, vec2 virtual_address )
 vec4 vtexel2D( vsampler2D sampler, vec2 virtual_address )
 {
     vec3 physical_address = vaddress( sampler, virtual_address );
-    ivec3 size = textureSize2DArray( sampler.page_cache, 0 );
-    ivec3 index = ivec3( physical_address.xy * vec2( size ), physical_address.z );
+    float page_size = exp2( float( sampler.bias_range.x ) );
+    ivec3 index = ivec3( physical_address.xy * page_size, physical_address.z );
     return texelFetch2DArray( sampler.page_cache, index, 0 );
 }
