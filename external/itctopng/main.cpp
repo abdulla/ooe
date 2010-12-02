c30bf61681843b44fc46b965f638b495d38c5958
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-12-03 00:49:32 +1100

    Added itctopng to extract PNGs from ITC files.

---
+++ external/itctopng/main.cpp
@@ -0,0 +1,52 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#include <iostream>
+#include <iterator>
+
+#include <cstring>
+
+#include "foundation/executable/program.hpp"
+#include "foundation/io/file.hpp"
+#include "foundation/io/memory.hpp"
+
+OOE_ANONYMOUS_BEGIN( ( ooe ) )
+
+bool launch( const std::string&, const std::string&, s32 argc, c8** argv )
+{
+    if ( argc != 3 )
+    {
+        std::cout << "Usage: " << argv[ 0 ] << " <input.itc> <output.png>\n";
+        return false;
+    }
+
+    u8 signature[] = { 0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a };
+    memory in( ( descriptor( argv[ 1 ] ) ) );
+    u8* begin = in.as< u8 >();
+    u8* end = begin + in.size();
+    u8* i = end;
+
+    // search from the end to get the largest image
+    for ( i -= sizeof( signature ); i >= begin; --i )
+    {
+        if ( !std::memcmp( i, signature, sizeof( signature ) ) )
+            break;
+    }
+
+    if ( i < begin )
+    {
+        std::cout << "File \"" << argv[ 1 ] << "\" does not contain a PNG image\n";
+        return false;
+    }
+
+    file file( descriptor( argv[ 2 ], descriptor::write_new ) );
+    file.write( i, std::distance( i, end ) );
+    return true;
+}
+
+OOE_ANONYMOUS_END( ( ooe ) )
+
+//--- main -----------------------------------------------------------------------------------------
+extern "C" s32 main( s32 argc, c8** argv/*, c8** envp*/ )
+{
+    return executable::launch( launch, argc, argv );
+}
