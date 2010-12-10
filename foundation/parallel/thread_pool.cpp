f7723b26800333d9fe20943dd83a80b35532613a
Abdulla Kamar <abdulla.kamar@gmail.com>
2010-12-10 22:16:39 +1100

    Revert "Modified thread_pool to be work-stealing."
    This reverts commit dbd56b341bb4b47ff0f203b2a6cfb08b62906d48.

--- foundation/parallel/thread_pool.cpp
+++ foundation/parallel/thread_pool.cpp
@@ -14,34 +14,25 @@ OOE_NAMESPACE_BEGIN( ( ooe ) )
 class thread_unit
 {
 public:
-    thread_unit( const std::string& name, thread_pool& pool )
+    thread_unit( const std::string& name )
         : state( true ), loads( 0 ), semaphore( 0 ), queue(),
-        thread( name, make_function( *this, &thread_unit::main ), &pool )
+        thread( name, make_function( *this, &thread_unit::main ), 0 )
     {
     }
 
-    void stop( void )
+    ~thread_unit( void )
     {
         state = false;
         semaphore.up();
         thread.join();
     }
 
-    void enqueue( const task_ptr& task )
+    void insert( const task_ptr& task )
     {
         queue.enqueue( task );
 
         if ( !loads++ )
             semaphore.up();
-    }
-
-    bool dequeue( task_ptr& task )
-    {
-        if ( !loads || !queue.dequeue( task ) )
-            return false;
-
-        --loads;
-        return true;
     }
 
 private:
@@ -52,13 +43,11 @@ private:
     ooe::queue< task_ptr > queue;
     ooe::thread thread;
 
-    void* main( void* pointer )
+    void* main( void* )
     {
-        thread_pool& pool = *static_cast< thread_pool* >( pointer );
-
         while ( state )
         {
-            for ( task_ptr task; dequeue( task ) || pool.snoop( task ); )
+            for ( task_ptr task; queue.dequeue( task ); --loads )
             {
                 task->state = task_base::error;
                 OOE_PRINT( "thread_pool \"" << thread.name() << "\"",
@@ -83,32 +72,14 @@ thread_pool::thread_pool( const std::string& name )
     for ( up_t i = 0, end = executable::cpu_cores(); i != end; ++i )
     {
         std::string unit_name = name + ' ';
-        vector.push_back( new thread_unit( unit_name << i, *this ) );
+        vector.push_back( new thread_unit( unit_name << i ) );
     }
-}
-
-thread_pool::~thread_pool( void )
-{
-    for ( vector_type::iterator i = vector.begin(), end = vector.end(); i != end; ++i )
-        i->as< thread_unit >()->stop();
 }
 
 void thread_pool::insert( const task_ptr& task )
 {
-    vector[ index ].as< thread_unit >()->enqueue( task );
+    vector[ index ].as< thread_unit >()->insert( task );
     ++index %= vector.size();
-}
-
-bool thread_pool::snoop( task_ptr& task )
-{
-    // TODO: start at index - 1, and iterate backwards
-    for ( vector_type::iterator i = vector.begin(), end = vector.end(); i != end; ++i )
-    {
-        if ( i->as< thread_unit >()->dequeue( task ) )
-            return true;
-    }
-
-    return false;
 }
 
 OOE_NAMESPACE_END( ( ooe ) )
