9de8d22f753c43c321244850f2d775030039d969
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-02 01:23:29 +1100

    Added memoryrpc_client binary.
    This is for stress testing connections to a IPC memory server.

---
+++ external/memoryrpc/client/main.cpp
@@ -0,0 +1,20 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#include "foundation/executable/program.hpp"
+#include "foundation/ipc/memory/client.hpp"
+
+OOE_ANONYMOUS_BEGIN( ( ooe ) )
+
+bool launch( const std::string&, const std::string&, s32, c8** )
+{
+    ipc::memory::client client( "/ooe" );
+    return true;
+}
+
+OOE_ANONYMOUS_END( ( ooe ) )
+
+//--- main -----------------------------------------------------------------------------------------
+extern "C" s32 main( s32 argc, c8** argv/*, c8** envp*/ )
+{
+    return executable::launch( launch, argc, argv );
+}
