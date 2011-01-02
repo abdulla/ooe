be93b2f1a7c826f62cf350e65af0298e8773cf19
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-03 00:06:24 +1100

    Modified build to not link with libtbbmalloc.

--- platform/module/FindTBB.cmake
+++ platform/module/FindTBB.cmake
@@ -1,7 +1,5 @@
 find_path( TBB_INCLUDE_DIRS tbb/tbb.h )
-find_library( TBB_LIBRARY tbb )
-find_library( TBBMALLOC_LIBRARY tbbmalloc )
-set( TBB_LIBRARIES ${TBB_LIBRARY} ${TBBMALLOC_LIBRARY} )
+find_library( TBB_LIBRARIES tbb )
 mark_as_advanced( TBB_LIBRARIES TBB_INCLUDE_DIRS )
 
 include( FindPackageHandleStandardArgs )
