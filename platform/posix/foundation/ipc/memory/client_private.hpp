9ca51bb8056006f7e4170464a21f7fc80314f855
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-06 15:06:14 +1100

    Oops, for to add this as part of a previous commit.

---
+++ platform/posix/foundation/ipc/memory/client_private.hpp
@@ -0,0 +1,16 @@
+/* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
+
+#ifndef OOE_PLATFORM_POSIX_FOUNDATION_IPC_MEMORY_CLIENT_PRIVATE_HPP
+#define OOE_PLATFORM_POSIX_FOUNDATION_IPC_MEMORY_CLIENT_PRIVATE_HPP
+
+#include "foundation/ipc/name.hpp"
+
+OOE_NAMESPACE_BEGIN( ( ooe )( platform )( ipc )( memory ) )
+
+void client_construct( ooe::ipc::memory::transport& )
+{
+}
+
+OOE_NAMESPACE_END( ( ooe )( platform )( ipc )( memory ) )
+
+#endif  // OOE_PLATFORM_POSIX_FOUNDATION_IPC_MEMORY_CLIENT_PRIVATE_HPP
