02f6084bd98b61aad8254344d065a4d8ac87d8cb
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-06 19:24:34 +1100

    Revert "Modified build to not link with libtbbmalloc."
    This reverts commit be93b2f1a7c826f62cf350e65af0298e8773cf19.

--- platform/module/FindTBB.cmake
+++ platform/module/FindTBB.cmake
@@ -1,5 +1,7 @@
 find_path( TBB_INCLUDE_DIRS tbb/tbb.h )
-find_library( TBB_LIBRARIES tbb )
+find_library( TBB_LIBRARY tbb )
+find_library( TBBMALLOC_LIBRARY tbbmalloc )
+set( TBB_LIBRARIES ${TBB_LIBRARY} ${TBBMALLOC_LIBRARY} )
 mark_as_advanced( TBB_LIBRARIES TBB_INCLUDE_DIRS )
 
 include( FindPackageHandleStandardArgs )
