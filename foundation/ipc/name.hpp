3b04c82c66e5c66a18a20ef1a265ec71f7684bd2
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-06 14:30:25 +1100

    Added server_name().

--- foundation/ipc/name.hpp
+++ foundation/ipc/name.hpp
@@ -3,14 +3,13 @@
 #ifndef OOE_FOUNDATION_IPC_NAME_HPP
 #define OOE_FOUNDATION_IPC_NAME_HPP
 
-#include "foundation/ipc/fundamental.hpp"
 #include "foundation/utility/macro.hpp"
 #include "foundation/utility/string.hpp"
 
 OOE_NAMESPACE_BEGIN( ( ooe )( ipc ) )
 
-std::string link_name( pid_t, time_t, link_t );
 std::string local_name( const std::string& ) OOE_VISIBLE;
+std::string server_name( const std::string& ) OOE_VISIBLE;
 std::string unique_name( void ) OOE_VISIBLE;
 
 OOE_NAMESPACE_END( ( ooe )( ipc ) )
