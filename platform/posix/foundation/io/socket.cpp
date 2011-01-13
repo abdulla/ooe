09421f783541ce59611461bfd7823bc0eea8f0c9
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-01-13 21:15:37 +1100

    Add socket::peer_id() and remove socket::option().

--- platform/posix/foundation/io/socket.cpp
+++ platform/posix/foundation/io/socket.cpp
@@ -1,5 +1,10 @@
 /* Copyright (C) 2010 Abdulla Kamar. All rights reserved. */
 
+#include <cerrno>
+
+#include <sys/socket.h>
+
+#include "foundation/io/error.hpp"
 #include "foundation/io/socket.hpp"
 
 OOE_NAMESPACE_BEGIN( ( ooe )( platform ) )
@@ -11,3 +16,19 @@ socket::socket( const descriptor& fd )
 }
 
 OOE_NAMESPACE_END( ( ooe )( platform ) )
+
+OOE_NAMESPACE_BEGIN( ( ooe ) )
+
+//--- socket ---------------------------------------------------------------------------------------
+socket::peer_tuple socket::peer_id( void ) const
+{
+    ucred credential;
+    u32 length = sizeof( credential );
+
+    if ( getsockopt( get(), SOL_SOCKET, SO_PEERCRED, &credential, &length ) )
+        throw error::io( "socket: " ) << "Unable to get peer id: " << error::number( errno );
+
+    return make_tuple( credential.uid, credential.gid );
+}
+
+OOE_NAMESPACE_END( ( ooe ) )
