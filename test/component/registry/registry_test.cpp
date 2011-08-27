26535f61dc6a5a3c08da676ac86eb995d734e6d3
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-08-27 20:25:30 +1000

    Modify registry test to use script hosts.

--- test/component/registry/registry_test.cpp
+++ test/component/registry/registry_test.cpp
@@ -22,6 +22,21 @@ OOE_ANONYMOUS_BEGIN( ( ooe ) )
 
 typedef scoped_ptr< scoped_fork > fork_ptr;
 
+//--- data -----------------------------------------------------------------------------------------
+void spawn( const std::string& executable, const std::string& script )
+{
+    fork_io fork;
+
+    if ( fork.is_child() )
+    {
+        OOE_PRINT( "spawn", fork_io::execute( executable, script.c_str(), NULL ) );
+        fork_io::exit( false );
+    }
+
+    OOE_CHECK( "fork.wait() == success", fork.wait() == fork_io::success );
+}
+
+//--- setup ----------------------------------------------------------------------------------------
 class setup
 {
 public:
@@ -51,8 +66,9 @@ private:
         if ( fork->is_child() )
         {
             executable::null_fd( STDOUT_FILENO );
-            OOE_IGNORE( fork_io::execute( path_ + "registry", "-u", name.c_str(), NULL ) );
-            fork_io::exit( true );
+            OOE_PRINT( "registry",
+                fork_io::execute( path_ + "registry", "-u", name.c_str(), NULL ) );
+            fork_io::exit( false );
         }
     }
 };
@@ -124,7 +140,10 @@ OOE_TEST void fixture_type::test< 3 >( setup& setup )
         fork_ptr( new scoped_fork ).swap( fork );
 
         if ( fork->is_child() )
+        {
             fork_io::execute( setup.path() + "hello", "-u", name.c_str(), NULL );
+            fork_io::exit( true );
+        }
     }
 
     registry registry;
@@ -137,43 +156,27 @@ OOE_TEST void fixture_type::test< 4 >( setup& setup )
 {
     std::cerr << "load module in lua\n";
 
-    lua::vm vm;
-    vm.setup( lua::component_setup );
-
-    std::string path = setup.path() + "../share/test/script.lua";
-    vm.load( "hello/script.lua", path );
-
-    std::cout <<
-        "Lua version: " << vm.version() << "\n"
-        "Lua VM size: " << vm.size() << " bytes\n";
+    std::string executable = setup.path() + "lua_host";
+    std::string script = setup.path() + "../share/test/script.lua";
+    spawn( executable, script );
 }
 
 OOE_TEST void fixture_type::test< 5 >( setup& setup )
 {
     std::cerr << "load module in javascript\n";
 
-    javascript::vm vm;
-    vm.setup( javascript::component_setup );
-
-    std::string path = setup.path() + "../share/test/script.js";
-    vm.load( "hello/script.js", path );
-
-    std::cout <<
-        "JavaScript version: " << vm.version() << "\n"
-        "JavaScript VM size: " << vm.size() << " bytes\n";
+    std::string executable = setup.path() + "javascript_host";
+    std::string script = setup.path() + "../share/test/script.js";
+    spawn( executable, script );
 }
 
 OOE_TEST void fixture_type::test< 6 >( setup& setup )
 {
     std::cerr << "load module in python\n";
 
-    python::vm vm;
-    vm.setup( python::component_setup );
-
-    std::string path = setup.path() + "../share/test/script.py";
-    vm.load( "hello/script.py", path );
-
-    std::cout << "Python version: " << vm.version() << '\n';
+    std::string executable = setup.path() + "python_host";
+    std::string script = setup.path() + "../share/test/script.py";
+    spawn( executable, script );
 }
 
 OOE_NAMESPACE_END( ( ooe )( unit ) )
