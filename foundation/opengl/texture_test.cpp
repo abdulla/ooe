b3ff9791e55500324aad64c60dfee7463ef5bc3f
Abdulla Kamar <abdulla.kamar@gmail.com>
2011-12-12 21:44:42 +1100

    Add image_test and opengl_test.

--- foundation/opengl/texture_test.cpp
+++ foundation/opengl/texture_test.cpp
@@ -3,10 +3,12 @@
 #include "component/ui/miscellany.hpp"
 #include "foundation/executable/library.hpp"
 #include "foundation/executable/program.hpp"
+#include "foundation/image/dds.hpp"
 #include "foundation/math/math.hpp"
 #include "foundation/visual/event_queue.hpp"
 #include "foundation/visual/graphics.hpp"
 #include "foundation/visual/view.hpp"
+#include "test/unit/check.hpp"
 #include "test/unit/group.hpp"
 
 OOE_ANONYMOUS_BEGIN( ( ooe ) )
@@ -14,31 +16,50 @@ OOE_ANONYMOUS_BEGIN( ( ooe ) )
 const f32 width = 640;
 const f32 height = 480;
 const u32 size = 256;
+const image_format::type format = image_format::rgba_dxt1;
 
 const c8 vertex_shader[] =
     "uniform mat4 projection;\n\
-    uniform float depth;\n\
     attribute vec2 vertex;\n\
+    varying vec2 texcoord;\n\
     \n\
     void main( void )\n\
     {\n\
-        gl_Position = projection * vec4( vertex, depth, 1 );\n\
+        gl_Position = projection * vec4( vertex, 0, 1 );\n\
+        texcoord = vertex;\n\
     }";
 
 const c8 fragment_shader[] =
     "#extension GL_EXT_gpu_shader4 : enable\n\
     \n\
-    uniform vec3 colour;\n\
-    varying out vec4 texture;\n\
+    uniform sampler2DArray sampler;\n\
+    uniform int index;\n\
+    varying vec2 texcoord;\n\
     \n\
     void main( void )\n\
     {\n\
-        texture = vec4( colour, 1. );\n\
+        gl_FragColor = texture2DArray( sampler, vec3( texcoord, index ) );\n\
     }";
+
+image make_image( u8 red, u8 green, u8 blue )
+{
+    image image( size, size, image_format::rgba_u8 );
+    u8 pixel_size = ooe::pixel_size( image );
+
+    for ( u8* i = image.as< u8 >(), * end = i + byte_size( image ); i != end; i += pixel_size )
+    {
+        i[ 0 ] = red;
+        i[ 1 ] = green;
+        i[ 2 ] = blue;
+        i[ 3 ] = 255;
+    }
+
+    return dxt::encode( image, format );
+}
 
 typedef unit::group< anonymous_t, anonymous_t, 1 > group_type;
 typedef group_type::fixture_type fixture_type;
-group_type group( "frame" );
+group_type group( "texture" );
 
 OOE_ANONYMOUS_END( ( ooe ) )
 
@@ -46,15 +67,19 @@ OOE_NAMESPACE_BEGIN( ( ooe )( unit ) )
 
 OOE_TEST void fixture_type::test< 0 >( anonymous_t& )
 {
-    std::cerr << "test frame buffer\n";
+    std::cerr << "test texture array\n";
 
     std::string root = executable::path()._0;
-    library library( root + "../lib/libopengl.so", library::global_lazy );
+    library library( root + "../lib/libopengl" OOE_EXTENSION, library::global_lazy );
 
     event_queue queue;
     view view( queue, width, height, false );
     device_ptr device = library.find< device_open_type >( "device_open" )( view, false );
-    device->set( device::depth_test, true );
+
+    const s32 array_size = device->limit( device::array_size );
+    texture_array_ptr texture_array =
+        device->texture_array( image_metadata( size, size, format ), array_size );
+    OOE_CHECK( "texture array size " << array_size << " > 0 ", array_size );
 
     shader_vector vector;
     vector.push_back( device->shader( vertex_shader, shader::vertex ) );
@@ -63,34 +88,25 @@ OOE_TEST void fixture_type::test< 0 >( anonymous_t& )
 
     block_ptr block = program->block( make_index( device ) );
     block->input( "vertex", block::f32_2, false, make_point( device ) );
+    block->input( "sampler", texture_array );
     block->input( "projection", orthographic( 0, width / height, 1, 0 ) );
 
-    image_metadata texture_metadata( width, height, image_format::rgb_u8 );
-    image_metadata target_metadata( width, height, image_format::depth_u24 );
-    texture_ptr texture = device->texture( texture_metadata, texture::nearest, false );
-    target_ptr target = device->target( target_metadata );
-
-    frame_ptr frame = program->frame( width, height );
-    frame->output( "texture", texture );
-    frame->output( target );
-
-    frame_ptr default_frame = device->default_frame( width, height );
-    default_frame->clear();
-
-    for ( s32 i = 0; i != 256; ++i )
+    image input[] =
     {
+        make_image( 255, 0, 0 ),
+        make_image( 0, 255, 0 ),
+        make_image( 0, 0, 255 )
+    };
+
+    frame_ptr frame = device->default_frame( width, height );
+
+    for ( s32 i = 0; i != array_size; ++i )
+    {
+        texture_array->write( input[ i % 3 ], 0, 0, i );
+        block->input( "index", i );
+
         frame->clear();
         device->draw( block, frame, 1 );
-
-        block->input( "depth", 1.f );
-        block->input( "colour", 0.f, 1.f, 0.f );
-        device->draw( block, frame, 1 );
-
-        block->input( "depth", 0.f );
-        block->input( "colour", 1.f, 0.f, 0.f );
-        device->draw( block, frame, 1 );
-
-        default_frame->write( frame );
         device->swap();
 
         event event;
