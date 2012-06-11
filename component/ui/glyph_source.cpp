2c25aa09df6e3f8524e63bb42a5829f6f8123613
Abdulla Kamar <abdulla.kamar@gmail.com>
2012-06-11 15:48:12 +1000

    Add changes to glyph_source.

--- component/ui/glyph_source.cpp
+++ component/ui/glyph_source.cpp
@@ -1,18 +1,79 @@
 /* Copyright (C) 2012 Abdulla Kamar. All rights reserved. */
 
+#include <boost/functional/hash.hpp>
+
+#include "component/ui/glyph_source.hpp"
+
+OOE_NAMESPACE_BEGIN( ( std )( tr1 ) )
+
+template< typename t0, typename t1 >
+    inline up_t hash< tuple< t0, t1 > >::operator ()( tuple< t0, t1 > tuple ) const
+{
+    up_t result = boost::hash< t0 >()( tuple._0 );
+    boost::hash_combine( result, tuple._1 );
+    return result;
+}
+
+OOE_NAMESPACE_END( ( std )( tr1 ) )
+
 OOE_ANONYMOUS_BEGIN( ( ooe ) )
 
 const u32 page_size_ = 256;
 const image_format::type format_ = image_format::rgba_u8;
+
+struct source_metric
+    : public font::metric
+{
+    bool valid;
+
+    source_metric( const font::metric& metric_, bool valid_ )
+        : metric( metric_ ), valid( valid_ )
+    {
+    }
+};
+
+c8 hyphenate( c8 c )
+{
+    return std::isspace( c ) ? '-' : std::tolower( c );
+}
+
+std::string get_root( const std::string& base, const font::face& face )
+{
+    std::string suffix = face.string( font::face::family ) + '-' + face.string( font::face::style );
+    std::transform( suffix.begin(), suffix.end(), suffix.begin(), hyphenate );
+    std::string path = canonical_path( base ) + '/' + suffix;
+
+    if ( !exists( path ) )
+        make_directory( path );
+
+    return path;
+}
+
+memory open_memory( const std::string& root, const font::face& face )
+{
+    descriptor desc( root + "/metric", descriptor::read_write );
+    up_t size = sizeof( source_metric ) * face.number( font::face::glyphs );
+
+    if ( desc.size() < size )
+        desc.resize( size );
+
+    return memory( desc, memory::read_write );
+}
+
+u32 get_size( const device_ptr& device )
+{
+    return device->limit( device::texture_size ) * page_size_;
+}
 
 OOE_ANONYMOUS_END( ( ooe ) )
 
 OOE_NAMESPACE_BEGIN( ( ooe ) )
 
 //--- glyph_source ---------------------------------------------------------------------------------
-glyph_source::glyph_source( const device_ptr& device, const font::face& face_ )
-    : mutex(), face( face_ ), index_rw(), index_cache(), kerning_rw(), kerning_cache(),
-    size_( device->limit( device::texture_size ) * page_size_ )
+glyph_source::glyph_source( const device_ptr& device, const font::face& face_,
+    const std::string& base )
+    : mutex(), face( face_ ), root( get_root( base, face ) ), memory( open_memory( root, face ) ),
+    index_cache( 1 << 9 ), kerning_cache( 1 << 14 ), size_( get_size( device ) )
 {
 }
 
@@ -37,54 +98,44 @@ image_format::type glyph_source::format( void ) const
 
 u32 glyph_source::glyph_index( u32 code_point ) const
 {
-    {
-        read_lock read_lock( index_rw );
-        index_type::const_iterator i = index_cache.find( code_point );
+    index_type::const_iterator i = index_cache.find( code_point );
 
-        if ( i != index_cache.end() )
-            return i->second;
-    }
+    if ( i != index_cache.end() )
+        return i->second;
 
     u32 index;
     {
         lock lock( mutex );
         index = face.glyph_index( code_point );
     }
-    {
-        write_lock lock( index_rw );
-        index_cache.insert( std::make_pair( code_point, index ) );
-    }
+
+    index_cache.insert( std::make_pair( code_point, index ) );
     return index;
 }
 
-u32 glyph_source::kerning( u32 left, u32 right ) const
+f32 glyph_source::kerning( u32 left, u32 right ) const
 {
     kerning_type::key_type key( left, right );
-    {
-        read_lock read_lock( kerning_rw );
-        kerning_type::const_iterator i = kerning_cache.find( key );
+    kerning_type::const_iterator i = kerning_cache.find( key );
 
-        if ( i != kerning_cache.end() )
-            return i->second;
-    }
+    if ( i != kerning_cache.end() )
+        return i->second;
 
-    u32 delta;
+    f32 delta;
     {
         lock lock( mutex );
         delta = face.kerning( left, right );
     }
-    {
-        write_lock lock( kerning_rw );
-        kerning_cache.insert( std::make_pair( key, delta ) );
-    }
+
+    kerning_cache.insert( std::make_pair( key, delta ) );
     return delta;
 }
 
 glyph_coord glyph_source::glyph_coord( u32 glyph_index, u16 size ) const
 {
     lock lock( mutex );
-    font::bitmap bitmap = face.bitmap( glyph_index, size );
-    return glyph_source( bitmap.metric );
+    font::metric metric = face.metric( glyph_index );
+    return glyph_source( metric );
 }
 
 OOE_NAMESPACE_END( ( ooe ) )
--- component/ui/glyph_source.hpp
+++ component/ui/glyph_source.hpp
@@ -9,10 +9,9 @@ OOE_NAMESPACE_BEGIN( ( ooe ) )
 struct glyph_coord
 {
     const font::metric& metric;
-    u32 x;
-    u32 y;
+    const rect& rect;
 
-    glyph_coord( const font::metric&, u32, u32 );
+    glyph_coord( const font::metric&, const rect& );
 };
 
 //--- glyph_source ---------------------------------------------------------------------------------
@@ -20,7 +19,7 @@ class glyph_source
     : public physical_source
 {
 public:
-    glyph_source( const device_ptr&, const font::face& );
+    glyph_source( const device_ptr&, const font::face&, const std::string& );
     virtual ~glyph_source( void );
 
     virtual u32 size( void ) const;
@@ -28,21 +27,23 @@ public:
     virtual image_format::type format( void ) const;
 
     u32 glyph_index( u32 ) const;
-    u32 kerning( u32, u32 ) const;
+    f32 kerning( u32, u32 ) const;
     glyph_coord glyph_coord( u32, u16 ) const;
 
 private:
     typedef fixed_cache< u32, u32 > index_type;
-    typedef fixed_cache< tuple< u32, u32 >, u32 > kerning_type;
+    typedef fixed_cache< tuple< u32, u32 >, f32 > kerning_type;
+    typedef std::tr1::unordered_map< tuple< u32, u16 >, const rect* > coord_type;
 
     mutable ooe::mutex mutex;
     const font::face& face;
+    const std::string root;
 
-    mutable read_write index_rw;
+    ooe::memory memory;
     mutable index_type index_cache;
-
-    mutable read_write kerning_rw;
     mutable kerning_type kerning_cache;
+    mutable coord_type coord_map;
+    ooe::bin bin;
 
     u32 size_;
 
